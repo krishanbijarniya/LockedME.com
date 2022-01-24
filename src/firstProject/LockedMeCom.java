f907fffaa1fb8402dabf14e2606642c8c69d76c3
krishan <kk9289@gmail.com>
2022-01-24 12:16:52 +0530

    Show name of project and developer with menu

---
+++ src/firstProject/LockedMeCom.java
@@ -0,0 +1,35 @@
+package firstProject;
+
+public class LockedMeCom {
+
+	public static void main(String[] args) {
+		showMenu();
+		
+
+	}
+	public static void showMenu() {
+		System.out.println("**********************************************************");
+		System.out.println("\t\t Welcomes You LockedMe.Com");
+		System.out.println("**********************************************************");
+		System.out.println("\n\n\t\t   @Developed by Krishan@");
+		System.out.println("**********************************************************");
+		System.out.println("\n\n\t 1. Display all the Files");
+		System.out.println("\t 2. Add a new File");
+		System.out.println("\t 3. Delete a File");
+		System.out.println("\t 4. Search a File");
+		System.out.println("\t 5. Exit");
+	}
+	
+	public static void getFiles() {
+		
+	}
+	public static void createFile() {
+		
+	}
+	public static void deleteFile() {
+		
+	}
+	public static void searchFile() {
+		
+	}
+}
